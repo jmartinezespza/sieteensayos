7b5bcc78264aa25d8faca2d2599f2693f036aa9b
Jhonny Martínez Espinoza <jmartinez@sysneo.pe>
2019-10-04 11:13:42 -0500

    commit inicial

---
+++ src/3- El problema de la tierra.mm
@@ -0,0 +1,264 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1569907997746" ID="ID_877008716" MODIFIED="1569908086050" TEXT="Problema de la Tierra">
+<node CREATED="1569908088824" ID="ID_1678753362" MODIFIED="1569908126016" POSITION="right" TEXT="Problema agrario y del indio">
+<node CREATED="1569991723889" ID="ID_199755426" MODIFIED="1569991732675" TEXT="Problema del Indio">
+<node CREATED="1569991240707" ID="ID_1878967895" MODIFIED="1569991277162" TEXT="Puntos de vista">
+<node CREATED="1569991284388" ID="ID_465628530" MODIFIED="1569991916557" TEXT="Socialista">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1569991463488" ID="ID_218428793" MODIFIED="1569991501630" TEXT="Econ&#xf3;mico-Social">
+<node CREATED="1569991559170" ID="ID_590638461" MODIFIED="1569991900788" TEXT="Derecho a la tierra">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+</node>
+<node CREATED="1569991289246" ID="ID_791985449" MODIFIED="1569991925914" TEXT="Criollo">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &quot;Misti&quot;
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1569991416696" ID="ID_551550293" MODIFIED="1569991425383" TEXT="Administrativo"/>
+<node CREATED="1569991425805" ID="ID_67539447" MODIFIED="1569991431757" TEXT="Pedag&#xf3;gico"/>
+<node CREATED="1569991432399" ID="ID_494379287" MODIFIED="1569991436004" TEXT="Etnico"/>
+<node CREATED="1569991437207" ID="ID_1730022854" MODIFIED="1569991453393" TEXT="Moral"/>
+</node>
+</node>
+</node>
+<node CREATED="1569991754956" ID="ID_705036043" MODIFIED="1569991763102" TEXT="Problema de la Tierra">
+<node CREATED="1569991797447" ID="ID_528601651" MODIFIED="1569991955954" TEXT="Relacionado con el problema del Indio">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1569992344021" ID="ID_1517636533" MODIFIED="1569992601489" TEXT="Clase Feudal">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1569992020084" ID="ID_498456551" MODIFIED="1570077025230" TEXT="Debi&#xf3; liquidarse">
+<icon BUILTIN="yes"/>
+<icon BUILTIN="hourglass"/>
+<node CREATED="1569992176918" ID="ID_1256803637" MODIFIED="1569992207848" TEXT="Independencia"/>
+<node CREATED="1569992208305" ID="ID_1745012861" MODIFIED="1569992225914" TEXT="Clase burguesa"/>
+<node CREATED="1569992226651" ID="ID_285099576" MODIFIED="1569992234331" TEXT="Clase capitalista"/>
+</node>
+<node CREATED="1569992455267" ID="ID_1730491489" MODIFIED="1569993098234" TEXT="No perdi&#xf3; su predominio">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1569992705615" ID="ID_1836471436" MODIFIED="1569992734726" TEXT="Desamortizaci&#xf3;n de la propiedad agraria"/>
+<node CREATED="1569993196521" ID="ID_1585265559" MODIFIED="1569993282734" TEXT="No hubo promoci&#xf3;n de peque&#xf1;a propiedad"/>
+</node>
+<node CREATED="1570075434945" ID="ID_1400202249" MODIFIED="1570075507548" TEXT="Expresiones feudalidad">
+<node CREATED="1570075442811" ID="ID_1721535227" MODIFIED="1570075890648" TEXT="Latifundio">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1570075482123" ID="ID_7965498" MODIFIED="1570075900392" TEXT="Servidumbre">
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+</node>
+<node CREATED="1570075699114" ID="ID_1260091122" MODIFIED="1570075760298" TEXT="No se puede liquidar la servidumbre sin liquidar el latifundio">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1570075833345" ID="ID_370203187" MODIFIED="1570076389999" TEXT="Es un problema Econ&#xf3;mico-Social">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1570075855263" ID="ID_270524450" MODIFIED="1570075931491" TEXT="Pol&#xed;tico">
+<icon BUILTIN="forward"/>
+</node>
+</node>
+<node CREATED="1570076303609" ID="ID_32645326" MODIFIED="1570076333513" TEXT="No es un problema T&#xe9;cnico-Agr&#xed;cola">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1570076431899" ID="ID_395619088" MODIFIED="1570076442067" TEXT="Agr&#xf3;nomos">
+<icon BUILTIN="forward"/>
+</node>
+</node>
+<node CREATED="1570076647510" ID="ID_1204474865" MODIFIED="1570076653835" TEXT="Soluciones">
+<node CREATED="1570076655603" ID="ID_1089744745" MODIFIED="1570076660352" TEXT="Liberal">
+<node CREATED="1570076661819" ID="ID_689398139" MODIFIED="1570076676546" TEXT="Fraccionamiento de los latifundios">
+<node CREATED="1570076694017" ID="ID_1495938723" MODIFIED="1570076900534" TEXT="Peque&#xf1;a propiedad">
+<icon BUILTIN="launch"/>
+</node>
+<node CREATED="1570076780091" ID="ID_675463804" MODIFIED="1570076876282" TEXT="Caracter&#xed;sticas">
+<icon BUILTIN="list"/>
+<node CREATED="1570076796398" ID="ID_721278440" MODIFIED="1570076801880" TEXT="Ortodoxa"/>
+<node CREATED="1570076802198" ID="ID_1016971641" MODIFIED="1570076811318" TEXT="Constitucional"/>
+<node CREATED="1570076811862" ID="ID_37026028" MODIFIED="1570076833696" TEXT="Democr&#xe1;tica"/>
+<node CREATED="1570076820911" ID="ID_663176061" MODIFIED="1570076828254" TEXT="Capitalista y burguesa"/>
+<node CREATED="1570077175856" ID="ID_934979055" MODIFIED="1570077186772" TEXT="Individualista"/>
+</node>
+</node>
+</node>
+<node CREATED="1570077337527" ID="ID_469446915" MODIFIED="1570077354767" TEXT="Para Mari&#xe1;tegui">
+<node CREATED="1570077357541" ID="ID_781125332" MODIFIED="1570077781131" TEXT="La hora de ensayar el m&#xe9;todo liberal ya ha pasado">
+<icon BUILTIN="broken-line"/>
+</node>
+<node CREATED="1570077626297" ID="ID_1797711545" MODIFIED="1570077938845" TEXT="Caracter peculiar">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1570077687322" ID="ID_1578893260" MODIFIED="1570077924524" TEXT="Supervivencia de la comunidad">
+<icon BUILTIN="hourglass"/>
+<icon BUILTIN="group"/>
+</node>
+<node CREATED="1570077721874" ID="ID_1170286168" MODIFIED="1570077906268" TEXT="Elementos de socialismo pr&#xe1;ctico">
+<icon BUILTIN="xmag"/>
+<node CREATED="1570077743635" ID="ID_851425730" MODIFIED="1570077749724" TEXT="Agricultura"/>
+<node CREATED="1570077750192" ID="ID_373447911" MODIFIED="1570077755413" TEXT="Vida Ind&#xed;gena"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1570078034121" ID="ID_1844276538" MODIFIED="1570078049793" TEXT="Colonialismo-Feudalismo">
+<node CREATED="1570078161206" ID="ID_1403389403" MODIFIED="1570078230347" TEXT="R&#xe9;gimen econ&#xf3;mico feudal">
+<node CREATED="1570078235809" ID="ID_1524863517" MODIFIED="1570078246247" TEXT="Expresiones">
+<node CREATED="1570078248004" ID="ID_562645350" MODIFIED="1570078258179" TEXT="Gamonalismo"/>
+<node CREATED="1570078258497" ID="ID_1755425371" MODIFIED="1570078265020" TEXT="Latifundio"/>
+<node CREATED="1570078265412" ID="ID_1209370462" MODIFIED="1570078272809" TEXT="Servidumbre"/>
+</node>
+<node CREATED="1570078283222" ID="ID_1752289915" MODIFIED="1570078289136" TEXT="Se debe liquidar"/>
+</node>
+<node CREATED="1570078464636" ID="ID_1558945694" MODIFIED="1570078476023" TEXT="El virreynato sobrevive en el Feudalismo"/>
+<node CREATED="1570078875086" ID="ID_817557356" MODIFIED="1570079067921" TEXT="La subsistencia del feudalismo retraza el desarrollo capitalista">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1570079516076" ID="ID_1573651750" MODIFIED="1570079556303" TEXT="El problema ind&#xed;gena se subordina al problema de la tierra"/>
+<node CREATED="1570079587011" ID="ID_701237913" MODIFIED="1570079615553" TEXT="El pueblo inkaico">
+<node CREATED="1570079616832" ID="ID_1441398365" MODIFIED="1570079643038" TEXT="Dedidacos a la Agricultura y Pastoreo"/>
+<node CREATED="1570079651683" ID="ID_964905666" MODIFIED="1570079663934" TEXT="&quot;La vida viene de la tierra&quot;"/>
+<node CREATED="1570079667472" ID="ID_220139834" MODIFIED="1570079991194" TEXT="Obras colectivas">
+<icon BUILTIN="group"/>
+<node CREATED="1570079673677" ID="ID_120458911" MODIFIED="1570079685531" TEXT="Canales de irrigaci&#xf3;n"/>
+<node CREATED="1570079685841" ID="ID_1069778641" MODIFIED="1570079704091" TEXT="Los andenes y terrazas"/>
+</node>
+<node CREATED="1570079712497" ID="ID_904522015" MODIFIED="1570079722037" TEXT="Culto a la Mama Pacha"/>
+<node CREATED="1570079758333" ID="ID_1183690017" MODIFIED="1570079777852" TEXT="Propiedad comunitaria de los campos"/>
+<node CREATED="1570079778202" ID="ID_623191263" MODIFIED="1570079890597" TEXT="Comunismo Inkaico">
+<icon BUILTIN="xmag"/>
+<node CREATED="1570079862958" ID="ID_1868174537" MODIFIED="1570079872817" TEXT="Comunismo Agrario">
+<icon BUILTIN="forward"/>
+</node>
+<node CREATED="1570079894455" ID="ID_406824674" MODIFIED="1570079971717" TEXT="No puede ser negado por ser Autocr&#xe1;tico">
+<icon BUILTIN="yes"/>
+</node>
+</node>
+<node CREATED="1570080172414" ID="ID_1994957601" MODIFIED="1570080217129" TEXT="Economia. (Cesar Ugarte)">
+<node CREATED="1570080256630" ID="ID_1658643873" MODIFIED="1570080824217" TEXT="Propiedad Colectiva de la Tierra">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1570080350412" ID="ID_99694949" MODIFIED="1570080719131" TEXT="Cultivable por el Ayllu">
+<icon BUILTIN="group"/>
+<node CREATED="1570080606849" ID="ID_1275900843" MODIFIED="1570080736542" TEXT="Familias emparentadas">
+<icon BUILTIN="family"/>
+</node>
+</node>
+<node CREATED="1570080378703" ID="ID_1903092729" MODIFIED="1570080775015" TEXT="Lotes individuales intransferibles">
+<icon BUILTIN="gohome"/>
+</node>
+</node>
+<node CREATED="1570080494676" ID="ID_483869842" MODIFIED="1570080830654" TEXT="Propiedad colectiva de las agua, tierras de pasto y bosques">
+<icon BUILTIN="group"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1570080539636" ID="ID_283487687" MODIFIED="1570080548681" TEXT="Marca o Tribu">
+<node CREATED="1570080627021" ID="ID_204064413" MODIFIED="1570080640992" TEXT="Federaci&#xf3;n de Ayllus"/>
+</node>
+</node>
+<node CREATED="1570080652648" ID="ID_1649072675" MODIFIED="1570080850273" TEXT="Cooperaci&#xf3;n com&#xfa;n en el trabajo">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1570080669554" ID="ID_676116658" MODIFIED="1570080846589" TEXT="Apropiaci&#xf3;n individual de las cosechas y frutos">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1570080943399" ID="ID_1230226728" MODIFIED="1570080999827" TEXT="La economia inkaica no fu&#xe9; sustituida por una superior">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1570081259199" ID="ID_565456270" MODIFIED="1570081270395" TEXT="Aristocracia ind&#xed;gena">
+<node CREATED="1570081277013" ID="ID_1904374948" MODIFIED="1570081293822" TEXT="Estado eficiente y org&#xe1;nico">
+<node CREATED="1570081504037" ID="ID_1336113517" MODIFIED="1570081516961" TEXT="Hecho hist&#xf3;rico, positivo, material"/>
+</node>
+<node CREATED="1570081409016" ID="ID_1672539122" MODIFIED="1570081417119" TEXT="Diez millones de hombres">
+<node CREATED="1570081584253" ID="ID_1505080788" MODIFIED="1570081593549" TEXT="Subsistencia y crecimiento"/>
+</node>
+</node>
+<node CREATED="1570081309123" ID="ID_716564851" MODIFIED="1570081322469" TEXT="Bajo una Aristocracia extranjera">
+<node CREATED="1570081323978" ID="ID_76455216" MODIFIED="1570081342782" TEXT="Servidumbre"/>
+<node CREATED="1570081419193" ID="ID_1965483188" MODIFIED="1570081434324" TEXT="Un millos de hombres"/>
+</node>
+</node>
+<node CREATED="1570081645798" ID="ID_125954513" MODIFIED="1570081707943" TEXT="El coloniaje injert&#xf3; elementos de econom&#xed;a esclavista">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1570082060989" ID="ID_855816734" MODIFIED="1570082089613" TEXT="Pol&#xed;tica del coloniaje">
+<node CREATED="1570082091146" ID="ID_942224455" MODIFIED="1570082097875" TEXT="Despoblaci&#xf3;n">
+<node CREATED="1570082108705" ID="ID_1431959548" MODIFIED="1570082179693" TEXT="No se supo gobernar &quot;Gobernar es poblar&quot;"/>
+<node CREATED="1570082183554" ID="ID_94284685" MODIFIED="1570082196884" TEXT="R&#xe9;gimen de despoblaci&#xf3;n"/>
+<node CREATED="1570082269183" ID="ID_140040848" MODIFIED="1570082279455" TEXT="El pa&#xed;s se empobrecia y desangraba"/>
+</node>
+<node CREATED="1570082098228" ID="ID_995043959" MODIFIED="1570082103364" TEXT="Esclavitud">
+<node CREATED="1570082336343" ID="ID_342543440" MODIFIED="1570082422006" TEXT="Persecuci&#xf3;n y esclavizamiento del Indio">
+<node CREATED="1570157891433" ID="ID_1310579831" MODIFIED="1570157901959" TEXT="Sistema de &quot;Mitas&quot;"/>
+</node>
+<node CREATED="1570082428766" ID="ID_1372208465" MODIFIED="1570082462490" TEXT="No se considero el &quot;Capital humano&quot;"/>
+<node CREATED="1570082485628" ID="ID_417514500" MODIFIED="1570082502144" TEXT="Importaci&#xf3;n de esclavos">
+<node CREATED="1570158667627" ID="ID_1167112544" MODIFIED="1570159103367" TEXT="La esclavitud no es productivo">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1570158893778" ID="ID_1976196727" MODIFIED="1570158913887" TEXT="Cesar Ugarte">
+<node CREATED="1570158978965" ID="ID_1621036075" MODIFIED="1570159032733" TEXT="&quot;El Per&#xfa; no necesitaba brazos sino hombres&quot;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="idea"/>
+<icon BUILTIN="help"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1570159185538" ID="ID_1283245189" MODIFIED="1570159193540" TEXT="El Colonizador Espa&#xf1;ol">
+<node CREATED="1570161080965" ID="ID_538171584" MODIFIED="1570161096995" TEXT="La Feudalidad es la tara que nos dej&#xf3; el coloniaje">
+<node CREATED="1570161156062" ID="ID_1876297078" MODIFIED="1570161168749" TEXT="Se junt&#xf3; con la tara del esclavismo"/>
+</node>
+<node CREATED="1570161368566" ID="ID_1614046432" MODIFIED="1570161375231" TEXT="Solo buscaba oro"/>
+<node CREATED="1570161376709" ID="ID_797404143" MODIFIED="1570161391396" TEXT="No era creador de riqueza"/>
+<node CREATED="1570161937155" ID="ID_1662161064" MODIFIED="1570161973938" TEXT="&#xda;nicos n&#xfa;cleos de producci&#xf3;n">
+<node CREATED="1570161975834" ID="ID_1230009916" MODIFIED="1570161981599" TEXT="Jesuitas y Dominicos"/>
+</node>
+</node>
+<node CREATED="1570162577571" ID="ID_399990956" MODIFIED="1570162592137" TEXT="La comunidad bajo el coloniaje">
+<node CREATED="1570165424867" ID="ID_647627456" MODIFIED="1570165429714" TEXT="Las leyes de indias">
+<node CREATED="1570165430914" ID="ID_1469562994" MODIFIED="1570165456247" TEXT="Propiedad ind&#xed;gena"/>
+<node CREATED="1570165456733" ID="ID_860915580" MODIFIED="1570165463849" TEXT="Organizaci&#xf3;n comunista"/>
+</node>
+<node CREATED="1570165524940" ID="ID_1425294559" MODIFIED="1570165538690" TEXT="Propiedad feudal - Propiedad comunitaria">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1570165823540" ID="ID_578450998" MODIFIED="1570165844551" TEXT="Las &quot;Encomiendas&quot;">
+<node CREATED="1570165845096" ID="ID_303469382" MODIFIED="1570165851295" TEXT="Cobro de tributos"/>
+<node CREATED="1570165877449" ID="ID_874639987" MODIFIED="1570165895302" TEXT="Se apoderaban de las tierras de las comunidades"/>
+<node CREATED="1570165859065" ID="ID_717835058" MODIFIED="1570165875137" TEXT="Era en la pr&#xe1;ctica como un se&#xf1;or feudal"/>
+</node>
+</node>
+<node CREATED="1570166699303" ID="ID_1081116634" MODIFIED="1570166707862" TEXT="La revoluci&#xf3;n - independencia">
+<node CREATED="1570166709715" ID="ID_458909945" MODIFIED="1570166728217" TEXT="Clases seg&#xfa;n Echeverria">
+<node CREATED="1570166733313" ID="ID_1339228850" MODIFIED="1570166750693" TEXT="Togado, Clero y Mandones">
+<node CREATED="1570166830520" ID="ID_1721303290" MODIFIED="1570166851085" TEXT="Aristocracia, espa&#xf1;oles y algunos americanos"/>
+</node>
+<node CREATED="1570166751075" ID="ID_584915765" MODIFIED="1570166769941" TEXT="Los enriquecidos">
+<node CREATED="1570166877014" ID="ID_664092881" MODIFIED="1570166896279" TEXT="En los cabildos, Clase media"/>
+</node>
+<node CREATED="1570166770349" ID="ID_224271310" MODIFIED="1570166802179" TEXT="Los villanos (gauchos, rotos, cholos, etc)">
+<node CREATED="1570166917483" ID="ID_1204309514" MODIFIED="1570166924573" TEXT="Productora, trabajo manual"/>
+</node>
+</node>
+<node CREATED="1570167267527" ID="ID_1861779402" MODIFIED="1570167292148" TEXT="La clase aristocr&#xe1;tica feudal sigui&#xf3; dominando"/>
+</node>
+</node>
+</node>
+</map>
