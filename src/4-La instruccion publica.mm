eb53e7ed37c1f8862f86af34c20c1a275d6b69a7
Jhonny Martinez Espinoza <jhon18.26@gmail.com>
2019-10-30 21:19:58 -0500

    cambios

---
+++ src/4-La instruccion publica.mm
@@ -0,0 +1,29 @@
+<map version="1.1.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1570772763597" ID="ID_874089338" MODIFIED="1570772830812" TEXT="La instrucci&#xf3;n P&#xfa;blica">
+<node CREATED="1570772833615" ID="ID_550863114" MODIFIED="1570772846695" POSITION="right" TEXT="Influencias">
+<node CREATED="1570772867674" ID="ID_278103011" MODIFIED="1570772871198" TEXT="Espa&#xf1;ola">
+<node CREATED="1570772926356" ID="ID_1138221216" MODIFIED="1570772932840" TEXT="Tiene un dominio completo"/>
+</node>
+<node CREATED="1570772871642" ID="ID_1000643263" MODIFIED="1570772896617" TEXT="Francesa"/>
+<node CREATED="1570772897046" ID="ID_172602955" MODIFIED="1570772904496" TEXT="Norteamericana"/>
+</node>
+<node CREATED="1570773010902" ID="ID_1063347017" MODIFIED="1570773103639" POSITION="right" TEXT="Hay una superposici&#xf3;n de elementos extranjeros"/>
+<node CREATED="1570773110746" ID="ID_379870065" MODIFIED="1570773131599" POSITION="right" TEXT="Somos un pueblo sin fusionarse a&#xfa;n">
+<node CREATED="1570773136994" ID="ID_342172299" MODIFIED="1570773144311" TEXT="Ind&#xed;genas y conquistados"/>
+</node>
+<node CREATED="1570773201412" ID="ID_47847249" MODIFIED="1570773217913" POSITION="right" TEXT="Existe una educaci&#xf3;n sin esp&#xed;ritu nacional">
+<node CREATED="1570773272790" ID="ID_1435784247" MODIFIED="1570773286326" TEXT="Tiene un esp&#xed;ritu colonial y colonizador"/>
+<node CREATED="1570773287462" ID="ID_1766655151" MODIFIED="1570773319292" TEXT="Se consideraba a los indios como una raza inferior"/>
+</node>
+<node CREATED="1570773366481" ID="ID_1190843205" MODIFIED="1570773377318" POSITION="right" TEXT="La cultura era un privilegio de casta">
+<node CREATED="1570773379482" ID="ID_1482465426" MODIFIED="1570773420280" TEXT="Sentido aristocr&#xe1;tico, eclesi&#xe1;stico y literario"/>
+<node CREATED="1570773435147" ID="ID_830886041" MODIFIED="1570773446592" TEXT="El pueblo no tenia derecho a la educaci&#xf3;n"/>
+<node CREATED="1570773448211" ID="ID_1447487253" MODIFIED="1570773468705" TEXT="Formaci&#xf3;n de cl&#xe9;rigos y doctores"/>
+</node>
+<node CREATED="1570773762685" ID="ID_1367259574" MODIFIED="1570773790042" POSITION="right" TEXT="Gratuidad de la ense&#xf1;anza. 1831">
+<node CREATED="1570773791462" ID="ID_316736457" MODIFIED="1570773815539" TEXT="Era para ayudar a las familias antes pudientes"/>
+<node CREATED="1570773818205" ID="ID_39919900" MODIFIED="1570773844309" TEXT="No era para dirigido hacia el pueblo"/>
+</node>
+</node>
+</map>
